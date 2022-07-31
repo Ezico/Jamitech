c0f90aba1eab02a83789010e0547744079be95a8
ezico <greenportal0@gmail.com>
2022-07-31 10:33:39 +0100

    security changes

--- src/Contentful.js
+++ src/Contentful.js
@@ -6,6 +6,6 @@ const contentful = require("contentful");
 // });
 
 export default contentful.createClient({
-  space: "b7i72q1nelhe",
-  accessToken: "s65hYoO_o-8Vo6ZqK04hyAVYm1J2huFb4wdMjnT1_ZI",
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
